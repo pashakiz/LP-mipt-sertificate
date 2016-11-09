87d90fcb354742fdf0f8b74614577822f39e28a8
Pavel Kizernis <pashakiz@gmail.com>
2016-11-09 11:15:26 +0300

    init

---
+++ js/common.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+	//Placeholder
+	//Doc: https://github.com/NV/placeholder.js/
+	$("[placeholder]").textPlaceholder();
+
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("form").submit(function() {
+
+		var name = $(this).find("input[name='name']").val(),
+			email = $(this).find("input[name='email']").val();
+
+		if (!name || !email) {
+			alert("Заполните поля формы.");
+			return false;
+		}
+
+		var email_regexp = /.+@.+\..+/i;
+		var email_test = email_regexp.test(email);
+		if (!email_test) {
+			alert("Введен некоректный Email-адрес.");
+			return false;
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize(),
+			success: function(response) {
+				//$('#order_status').html(response);
+				$('#order_status').html('Спасибо, Ваша заявка отправлена!');
+				console.log("jquery-ajax-mail-success");
+				console.log('response: ' + response);
+			},
+			error:  function(xhr, str){
+				alert('Возникла ошибка: ' + xhr.responseCode);
+				console.log('response: ' + xhr);
+			}
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
+
+});
